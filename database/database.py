a75c0db19ebc49ebdc84dc0df762c7a1f38db075
U-KAMI\June <jhua@bu.edu>
2018-09-11 16:34:17 -0400

    updated add_source, automatically generates new source number

--- database/database.py
+++ database/database.py
@@ -45,23 +45,20 @@ def save_graph(time, temp, humidity, userid, source):
     plt.ylabel('Relative Humidity', fontsize=16)
     plt.savefig('../tmp_files/'+userid+'_'+str(source)+'_hum.png')
 
-def add_source(userid, source, label):
+def add_source(userid, label):
     connection = sqlite3.connect("data.db") 
     cur = connection.cursor()
 
-    # Check if source unique to userid
+    # Get new source number
     command_get_sources = ''' SELECT DISTINCT source from temprh WHERE userid=? '''
     cur.execute(command_get_sources, (userid,))
     distinct_sources = cur.fetchall()
-    distinct_sources = [i[0] for i in distinct_sources]
-    if source in distinct_sources:
-        print("ERROR: Source for user already exists!")
-        return
+    new_source = len(distinct_sources) + 1
 
     # Insert source for userid into table by loggin temp and humidity for 24 hours
     command = ''' INSERT into temprh(userid,source,label,time,temp,humidity) VALUES(?,?,?,?,?,?); '''
     for i in range(1,25):
-       cur.execute(command, (userid, source, label, i, i+1, i+2))
+       cur.execute(command, (userid, new_source, label, i, i+1, i+2))
 
     connection.commit()
 
@@ -82,7 +79,7 @@ if __name__ == "__main__":
     cur = connection.cursor()
     cur.execute(create_table)
     
-    add_source('jhua', 1, 'ma first source')
+    add_source('jhua', 'ma first source')
     tempHumidity = query('jhua', 1, 1, 100)
     temp = [i[0] for i in tempHumidity]
     humidity = [i[1] for i in tempHumidity]
@@ -90,9 +87,9 @@ if __name__ == "__main__":
     save_graph(range(1,25), temp, humidity, 'jhua', 1) 
 
 
-    add_source('jhua', 3, 'ma first source')
-    add_source('jhua', 4, 'ma twoth source')
-    add_source('aa', 1, 'ma first source')
-    add_source('aa', 7, 'ma first source')
+    add_source('jhua', 'ma first source')
+    add_source('jhua', 'ma twoth source')
+    add_source('aa', 'ma first source')
+    add_source('aa', 'ma first source')
     get_label('jhua', 1)
     

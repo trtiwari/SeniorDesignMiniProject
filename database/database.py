ece36014f295f7168042e9fd661454c986c102f8
U-KAMI\June <jhua@bu.edu>
2018-09-11 01:32:55 -0400

    Updated database to get sources, add sources; Updated display_results python file; Added source label to database.py

---
+++ database/database.py
@@ -0,0 +1,91 @@
+import sqlite3
+import matplotlib.pyplot as plt
+
+def query(userid, source, time_start, time_end):
+    # Get temp and humidity for the time range [<time_start>, <time_end>] for specified <userid> and <source>
+    output = []
+    cur = sqlite3.connect("data.db").cursor()
+    for time in range(time_start, time_end+1):
+        command = '''SELECT temp, humidity from temprh WHERE userid=? and source=? and time=?'''
+        cur.execute(command, (userid, source, time))
+        result = cur.fetchone()
+        if result is not None:
+            output.append(result)
+    return(output)
+
+def get_sources(userid):
+    # select unique source, label pairs
+    command = '''SELECT DISTINCT source, label from temprh WHERE userid=?'''
+    cur = sqlite3.connect("data.db").cursor()
+    cur.execute(command, (userid,))
+    result = cur.fetchall()
+    return result
+
+def save_graph(time, temp, humidity, userid, source):
+    # plot temp
+    plt.figure(figsize=(10,8))
+    plt.plot(time, temp, color='red')
+    plt.title('Temperature', fontsize=18)
+    plt.xlabel('Hour', fontsize=16)
+    plt.ylabel('Fahrenheit', fontsize=16)
+    plt.savefig(userid+'_'+str(source)+'temp.png')
+
+    # plot humidity
+    plt.figure(figsize=(10,8))
+    plt.plot(time, humidity, color='blue')
+    plt.title('Humidity', fontsize=18)
+    plt.xlabel('Hour', fontsize=16)
+    plt.ylabel('Relative Humidity', fontsize=16)
+    plt.savefig(userid+'_'+str(source)+'_hum.png')
+
+def add_source(userid, source, label):
+    connection = sqlite3.connect("data.db") 
+    cur = connection.cursor()
+
+    # Check if source unique to userid
+    command_get_sources = ''' SELECT DISTINCT source from temprh WHERE userid=? '''
+    cur.execute(command_get_sources, (userid,))
+    distinct_sources = cur.fetchall()
+    distinct_sources = [i[0] for i in distinct_sources]
+    if source in distinct_sources:
+        print("ERROR: Source for user already exists!")
+        return
+
+    # Insert source for userid into table by loggin temp and humidity for 24 hours
+    command = ''' INSERT into temprh(userid,source,label,time,temp,humidity) VALUES(?,?,?,?,?,?); '''
+    for i in range(1,25):
+       cur.execute(command, (userid, source, label, i, i+1, i+2))
+
+    connection.commit()
+
+if __name__ == "__main__":
+
+    connection = sqlite3.connect("data.db")
+
+    create_table = ''' CREATE TABLE IF NOT EXISTS temprh (
+        userid text NOT NULL,
+        source integer NOT NULL,
+        label text NOT NULL, 
+        time integer,
+        temp integer,
+        humidity real,
+        PRIMARY KEY (userid, source, time)
+    ); '''
+
+    cur = connection.cursor()
+    cur.execute(create_table)
+    
+    add_source('jhua', 1, 'ma first source')
+    tempHumidity = query('jhua', 1, 1, 100)
+    temp = [i[0] for i in tempHumidity]
+    humidity = [i[1] for i in tempHumidity]
+  
+    save_graph(range(1,25), temp, humidity, 'jhua', 1) 
+
+
+    add_source('jhua', 3, 'ma first source')
+    add_source('jhua', 4, 'ma twoth source')
+    add_source('aa', 1, 'ma first source')
+    add_source('aa', 7, 'ma first source')
+    get_sources('jhua')
+    
--- server_code/display_results/display_results.py
+++ server_code/display_results/display_results.py
@@ -1,7 +1,34 @@
+import os
+import time
+import database as db 
 
-def display_results():
+def display_results(userid, source, label):
+	# query last 24 hours and save graphs
+	queryResults = db.query(userid, source, 1, 24)
+	temp = [i[0] for i in queryResults]
+    hum = [i[1] for i in queryResults]
+    save_graph(range(1,25), temp, hum, userid, source)
+
 	with open("display_results.template.html",'r') as descriptor:
 		template = descriptor.read()
-		filename = "trtiwari_livingroom.png"
-		template.find("{{tem}}")
 
+		# Get user + source specific files
+		temp_path = userid+'_'+source+'_temp.png'
+		hum_path = userid+'_'+source+'_hum.png'
+
+		# Check if image paths exist
+		if not os.path.exists(temp_path) or not os.path.exists(hum_path):
+			print('ERROR: Cannot find png file(s)!')
+			return
+
+		### Find and Replace HTML variables
+		find_temp = template.find("{{temp}}")
+		find_hum = template.find("{{humidity}}")
+		find_label = template.find("{{source_label}}")
+
+		if find_temp!=-1 and find_hum!=-1 and find_label!=-1:
+			template = template.replace("{{temp}}", file_temp)
+			template = template.replace("{{humidity}}", file_hum)
+			template = template.replace("{{source_label}}", find_label)
+		else: 
+			print("ERROR: HTML variable missing!")

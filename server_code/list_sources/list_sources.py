fd0a94e40b736c009ffc391b07023c6484fe1c98
U-KAMI\June <jhua@bu.edu>
2018-09-11 17:03:18 -0400

    Added list_sources html and python file

--- server_code/list_sources/list_sources.py
+++ server_code/list_sources/list_sources.py
@@ -1,10 +1,29 @@
+from database import database as db 
 
 def list_sources(user_id):
-	sources_labels = get_sources(user_id)
+	sources_labels = db.get_sources(user_id)
 	sources = [i[0] for i in sources_labels]
 	labels = [i[1] for i in sources_labels]
 
 	with open("sources.template.html",'r') as descriptor:
 		template = descriptor.read()
+
+
+		box = '''
+				<div class = "box" onclick = "location.href='{{url}}'">
+				<h2>{{source_label}}</h2>
+				</div>
+			'''
+
+		all_boxes = ""
+
+		for i in range(len(sources)):
+			url = userid+"/display_results/"+sources[i]
+			source_label = labels[i]
+			mod_box = box.replace("{{url}}", url)
+			mod_box = mod_box.replace("{{source_label}}", source_label)
+			all_boxes = all_boxes + mod_box
+
+		template.replace('{{list}}', all_boxes)
 
 	return template

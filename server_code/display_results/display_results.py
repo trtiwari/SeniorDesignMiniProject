0e436a94c9bc6a483b6985f87c1bf56e210886a3
U-KAMI\June <jhua@bu.edu>
2018-09-19 13:54:22 -0400

    Commented and error checked display_results and list_sources python files

--- server_code/display_results/display_results.py
+++ server_code/display_results/display_results.py
@@ -1,29 +1,47 @@
 import os
-import time
 from database import database as db 
 
 def display_results(DOC_ROOT, userid, source):
+	'''
+	Display temp and hum graph for source belonging to user.
+
+	Args:
+		DOC_ROOT (str): Path to project directory
+        userid (str): Unique id for user
+        source (int): Source number
+
+	Returns:
+		template (str): Updated HTML template
+	'''
 	# query last 24 hours and save graphs
 	queryResults = db.query(DOC_ROOT, userid, source, 0, 23)
 	temp = [i[0] for i in queryResults]
 	hum = [i[1] for i in queryResults]
 	db.save_graph(DOC_ROOT, range(24), temp, hum, userid, source)
 
+	# Modify html file
 	with open(DOC_ROOT+"/frontend/html/display_results.template.html",'r') as descriptor:
 		template = descriptor.read()
 
 		# Get user + source specific files
 		temp_path = '/tmp_files/'+userid+'_'+source+'_temp.png'
 		hum_path = '/tmp_files/'+userid+'_'+source+'_hum.png'
-		# Get label
+		# Get label of source
 		label = db.get_label(DOC_ROOT, userid, source)
 
 		### Find and Replace HTML variables
+		# Check that images exist
+		if not os.path.isfile(DOC_ROOT + temp_path) or not os.path.isfile(DOC_ROOT + hum_path):
+			print('ERROR: Image path does not exist!')
+			return template;
+		
+
+		# Check if variables in html file
 		find_temp = template.find("{{temperature.png}}")
 		find_hum = template.find("{{humidity.png}}")
 		find_label = template.find("{{source_label}}")
-
 		if find_temp!=-1 and find_hum!=-1 and find_label!=-1:
+			# Replace variables
 			template = template.replace("{{temperature.png}}", temp_path)
 			template = template.replace("{{humidity.png}}", hum_path)
 			template = template.replace("{{source_label}}", label)
--- server_code/list_sources/list_sources.py
+++ server_code/list_sources/list_sources.py
@@ -1,7 +1,17 @@
 from database import database as db 
 
 def list_sources(DOC_ROOT, userid):
-	
+	'''
+	List all user sources.
+
+	Args:
+		DOC_ROOT (str): Path to project directory
+        userid (str): Unique id for user
+
+	Returns:
+		template (str): Updated HTML template
+	'''
+	# Get sources and their respective labels
 	sources_labels = db.get_sources(DOC_ROOT, userid)
 	sources = [i[0] for i in sources_labels]
 	labels = [i[1] for i in sources_labels]
@@ -9,14 +19,15 @@ def list_sources(DOC_ROOT, userid):
 	with open(DOC_ROOT+"/frontend/html/list_sources.template.html",'r') as descriptor:
 		template = descriptor.read()
 
+		# HTML template for listing sources
 		box = '''
 				<div class = "box" onclick = "location.href='{{url}}'">
 				<h2>{{source_label}}</h2>
 				</div>
 			  '''
-
 		all_boxes = ""
 
+		# Add in new html div box for each source
 		if len(sources) != 0:
 			for i in range(len(sources)):
 				url = "display_results/"+str(sources[i])
@@ -30,9 +41,15 @@ def list_sources(DOC_ROOT, userid):
 						<h2>You have no sources!</h2>
 						</div>
 						'''
+		# Check html variables exists
+		find_list = template.find("{{list}}")
+		find_add_sources_url = template.find("{{add_sources_url}}")
+		if find_list == -1 or find_add_sources_url == -1:
+			print('ERROR: HTML variable missing!')
+			return template
 
+		# Replace HTML variables
 		template = template.replace('{{list}}', all_boxes)
-
 		template = template.replace('{{add_sources_url}}', userid + "/add_sources/")
 
 		return template

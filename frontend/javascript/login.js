009d8177568d5ca0ad23ca7c45f97a63cad3ec15
Trishita <trtiwari@bu.edu>
2018-09-19 19:21:02 -0400

    commented the javascript as well

--- frontend/javascript/login.js
+++ frontend/javascript/login.js
@@ -1,6 +1,8 @@
 function onSignIn(googleUser) {
+	// this is the Google Sign in call back function that is invoked as soon as a 
+	// user signs in
   var profile = googleUser.getBasicProfile();
-  var id = profile.getId(); // Do not send to your backend! Use an ID token instead.
+  var id = profile.getId();
   var name =  profile.getName();
   window.location.href = id +"/list_sources";
 }
--- frontend/javascript/logout.js
+++ frontend/javascript/logout.js
@@ -1,4 +1,5 @@
   function signOut() {
+    // Google sign out callback function invoked when the user signs out
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
@@ -7,6 +8,7 @@
   }
   
  function onLoad() {
+    // loading Google Sign in dependencies
       gapi.load('auth2', function() {
         gapi.auth2.init();
       });
